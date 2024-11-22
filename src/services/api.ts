5c0c34401034dac7eedb86bfc0ea9c9df88d59f8
Renan Johny Ortega <renanortega82@gmail.com>
2024-11-22 01:50:22 -0300

    build: add variable.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
-const API_BASE_URL = "https://localhost:44314/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+console.log(API_BASE_URL);
 
 export const api: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
