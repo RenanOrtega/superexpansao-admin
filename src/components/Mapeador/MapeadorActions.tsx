85308899f988d1a4e08b4218ffb2f76127a2818f
Renan Johny Ortega <renanortega82@gmail.com>
2024-11-27 15:05:13 -0300

    feat: change filter of mapeadores.

--- src/components/Mapeador/MapeadorActions.tsx
+++ src/components/Mapeador/MapeadorActions.tsx
@@ -6,7 +6,7 @@ import { MapeadorFormData } from "@/types/Mapeador";
 import { mapeadorService } from "@/services/mapeadorService";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { cn } from "@/lib/utils";
-import { CalendarIcon } from "lucide-react";
+import { CalendarIcon, Filter } from "lucide-react";
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import { Calendar } from "../ui/calendar";
@@ -18,6 +18,7 @@ import {
   SelectValue,
 } from "../ui/select";
 import { MapeadorFiltersProps } from "@/types/Mapeador/filters";
+import { DialogForm } from "../DialogForm";
 
 export function MapeadorActions({
   activeFilters,
@@ -29,6 +30,8 @@ export function MapeadorActions({
     vehicle: activeFilters.vehicle || "",
     lastMapping: activeFilters.lastMapping || "",
   });
+
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
 
   const [date, setDate] = useState<Date | undefined>(
     activeFilters.lastMapping ? new Date(activeFilters.lastMapping) : undefined
@@ -71,65 +74,6 @@ export function MapeadorActions({
 
   return (
     <div className="w-full space-y-4 md:space-y-0 mb-5">
-      <div className="flex flex-col md:flex-row gap-4 mb-4">
-        <Input
-          className="flex-1"
-          placeholder="Filtrar por nome"
-          value={filterForm.name}
-          onChange={(e) => handleFilterFormChange("name", e.target.value)}
-        />
-        <Input
-          className="flex-1"
-          placeholder="Filtrar por cidade"
-          value={filterForm.city}
-          onChange={(e) => handleFilterFormChange("city", e.target.value)}
-        />
-        <div className="flex-1">
-          <Select
-            onValueChange={(e) => handleFilterFormChange("vehicle", e)}
-            value={filterForm.vehicle}
-          >
-            <SelectTrigger>
-              <SelectValue placeholder="Filtre por veículo" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem className="cursor-pointer" value="Moto">
-                Moto
-              </SelectItem>
-              <SelectItem className="cursor-pointer" value="Carro">
-                Carro
-              </SelectItem>
-            </SelectContent>
-          </Select>
-        </div>
-        <Popover>
-          <PopoverTrigger asChild className="flex-1">
-            <Button
-              variant={"outline"}
-              className={cn(
-                "justify-start text-left font-normal",
-                !date && "text-muted-foreground"
-              )}
-            >
-              <CalendarIcon className="h-4 w-4" />
-              {date ? (
-                format(date, "PPP", { locale: ptBR })
-              ) : (
-                <span>Filtrar por mapeamento</span>
-              )}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-auto p-0">
-            <Calendar
-              mode="single"
-              selected={date}
-              onSelect={handleDateSelect}
-              initialFocus
-              locale={ptBR}
-            />
-          </PopoverContent>
-        </Popover>
-      </div>
       <div className="flex flex-col sm:flex-row justify-between gap-2">
         <div>
           <MapeadorCreateDialog
@@ -137,16 +81,89 @@ export function MapeadorActions({
           ></MapeadorCreateDialog>
         </div>
         <div className="flex flex-col sm:flex-row gap-2">
-          <Button
-            variant="outline"
-            onClick={handleClearFilters}
-            className="w-full sm:w-auto"
+          <DialogForm
+            open={isDialogOpen}
+            onOpenChange={setIsDialogOpen}
+            title="Filtrar Mapeador"
+            trigger={
+              <Button className="w-full sm:w-auto">
+                <Filter />
+                Filtrar
+              </Button>
+            }
           >
-            Limpar Filtros
-          </Button>
-          <Button onClick={handleApplyFilters} className="w-full sm:w-auto">
-            Filtrar
-          </Button>
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-col md:flex-row gap-4 md:gap-2">
+                <Input
+                  placeholder="Filtrar por nome"
+                  value={filterForm.name}
+                  onChange={(e) =>
+                    handleFilterFormChange("name", e.target.value)
+                  }
+                />
+                <Input
+                  placeholder="Filtrar por cidade"
+                  value={filterForm.city}
+                  onChange={(e) =>
+                    handleFilterFormChange("city", e.target.value)
+                  }
+                />
+              </div>
+              <Select
+                onValueChange={(e) => handleFilterFormChange("vehicle", e)}
+                value={filterForm.vehicle}
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Filtre por veículo" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem className="cursor-pointer" value="Moto">
+                    Moto
+                  </SelectItem>
+                  <SelectItem className="cursor-pointer" value="Carro">
+                    Carro
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+              <Popover>
+                <PopoverTrigger asChild className="flex-1">
+                  <Button
+                    variant={"outline"}
+                    className={cn(
+                      "justify-start text-left font-normal",
+                      !date && "text-muted-foreground"
+                    )}
+                  >
+                    <CalendarIcon className="h-4 w-4" />
+                    {date ? (
+                      format(date, "PPP", { locale: ptBR })
+                    ) : (
+                      <span>Filtrar por mapeamento</span>
+                    )}
+                  </Button>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0">
+                  <Calendar
+                    mode="single"
+                    selected={date}
+                    onSelect={handleDateSelect}
+                    initialFocus
+                    locale={ptBR}
+                  />
+                </PopoverContent>
+              </Popover>
+              <Button
+                variant="outline"
+                onClick={handleClearFilters}
+                className="w-full sm:w-auto"
+              >
+                Limpar Filtros
+              </Button>
+              <Button onClick={handleApplyFilters} className="w-full sm:w-auto">
+                Aplicar
+              </Button>
+            </div>
+          </DialogForm>
         </div>
       </div>
     </div>
