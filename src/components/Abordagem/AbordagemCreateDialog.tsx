5e8b558238eb46c4a90ac747b0b88bca16b0f9cc
Renan Ortega <renanortega82@gmail.com>
2024-12-10 10:23:06 -0300

    refactor: style button abordagem.

--- src/components/Abordagem/AbordagemCreateDialog.tsx
+++ src/components/Abordagem/AbordagemCreateDialog.tsx
@@ -62,7 +62,7 @@ export function AbordagemCreateDialog({
       open={isDialogOpen}
       onOpenChange={handleDialogChange}
       trigger={
-        <Button className="w-full sm:w-auto" variant="outline">
+        <Button className="w-full sm:w-auto">
           <Plus className="mr-2 h-4 w-4" /> Nova abordagem
         </Button>
       }
