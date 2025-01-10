f89374f8fa59b155e0c6d42eafdef710e47dd6f9
Renan Johny Ortega <renanortega.dev@gmail.com>
2025-01-09 22:33:45 -0300

    build: rename yaml.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 import { authService } from "./authService";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+const API_BASE_URL = "http://se-admin-lb-271985348.us-east-1.elb.amazonaws.com";
 
 const api: AxiosInstance = axios.create({
   baseURL: API_BASE_URL,
